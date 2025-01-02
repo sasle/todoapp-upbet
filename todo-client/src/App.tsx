5446127f714b302e9b5206190b3f42c328478567
sasle <sasle57@gmail.com>
2025-01-02 14:32:17 -0300

    feat: implement CORS support in backend and integrate Axios in frontend for API calls

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -1,3 +1,14 @@
+import { useCallback, useEffect } from "react";
+import api from "./services/api";
+
 export default function App() {
+  const fetchApi = useCallback(async () => {
+    await api.get("/");
+  }, []);
+
+  useEffect(() => {
+    fetchApi();
+  }, [fetchApi]);
+
   return <p>Todo Frontend</p>;
 }
