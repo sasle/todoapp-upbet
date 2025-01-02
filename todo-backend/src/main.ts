483647a7dcf99593a95305068d668396b1e3d468
sasle <sasle57@gmail.com>
2025-01-02 17:11:51 -0300

    feat: add Docker configuration for frontend and backend services

--- todo-backend/src/main.ts
+++ todo-backend/src/main.ts
@@ -5,12 +5,12 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
   app.enableCors(corsOptions);
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
